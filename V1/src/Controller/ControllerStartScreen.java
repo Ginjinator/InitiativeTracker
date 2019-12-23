0b1f9b813ef8a99cc39a5ef0948ce86b8c7105b6
Erik Ellis <elliserik69@gmail.com>
2019-12-22 21:26:19 -0500

    Added screens and controllers for the screens

--- V1/src/Controller/ControllerStartScreen.java
+++ V1/src/Controller/ControllerStartScreen.java
@@ -1,4 +1,22 @@
 package Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import java.io.IOException;
+
 public class ControllerStartScreen {
+    @FXML
+    public void getNames(ActionEvent event) throws IOException {
+
+    }
+
+    @FXML
+    public void createNames(ActionEvent event) throws IOException {
+
+    }
+
+    @FXML
+    public void addNames(ActionEvent event) throws IOException {
+
+    }
 }
