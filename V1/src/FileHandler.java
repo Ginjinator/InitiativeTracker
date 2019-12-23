9fb1d5e816e5ff9bc5e8419fe56dbaf1d409b989
Erik Ellis <elliserik69@gmail.com>
2019-12-22 21:02:36 -0500

    Initial working commit

---
+++ V1/src/FileHandler.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class FileHandler implements Serializable {
+
+    private ArrayList<String> characters;
+    private String characterNameFile = "CharactersNames.txt";
+    private String characterNamePath = "C:/Users/erike/IdeaProjects/IntiativeTracker/CharacterNames.txt";
+
+    public ArrayList<String> getCharacters() {
+        return characters;
+    }
+
+    public void setCharacters() throws IOException {
+        Scanner scanner = new Scanner(new File(characterNamePath));
+        characters = new ArrayList<String>();
+        while (scanner.hasNext()) {
+            characters.add(scanner.next());
+        }
+        scanner.close();
+    }
+}
